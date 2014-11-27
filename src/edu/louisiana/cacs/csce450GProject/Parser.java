79a3d19c4ec54bfff3cebc8ca5ec7ad67e501650
ajs6790 <alexjsyl@louisiana.edu>
2014-11-26 21:28:56 -0600

    Fresh setup

---
+++ src/edu/louisiana/cacs/csce450GProject/Parser.java
@@ -0,0 +1,37 @@
+package edu.louisiana.cacs.csce450Project;
+
+public class Parser{
+	/*
+	* YOUR CODE GOES HERE
+	* 
+	* You must implement two methods
+	* 1. parse
+	* 2. printParseTree
+     
+    * Print the intermediate states of the parsing process,
+    * including the intermediate states of the parse tree,make
+    * as specified in the class handout.
+    * If the input is legal according to the grammar,
+    * print ACCEPT, else UNGRAMMATICAL.
+    * If the parse is successful, print the final parse tree.
+
+	* You can modify the input and output of these function but not the name
+	*/
+	public Parser(String fileName){
+		System.out.println("File to parse : "+fileName);
+	}
+	/*
+	* Dummy code
+	*/
+	public void printParseTree(){
+		System.out.println("Hello World from " + getClass().getName());
+	}
+
+	/*
+	* Dummy code
+	*/
+	public void parse(){
+        printParseTree();
+	}
+
+}
